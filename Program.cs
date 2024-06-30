49e1181471f2776413f60e9436b3f7094de83ce9
Sofiia-radomanova <154997775+Sofiia-radomanova@users.noreply.github.com>
2024-06-30 13:35:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System; 
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter the notebook price: ");
+        double x = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the pen price: ");
+        double y = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the number of notebooks: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the number of pens: ");
+        int m = int.Parse(Console.ReadLine());
+
+        double overall = x*n + y*m;
+
+        Console.WriteLine("The total price of {0} notebooks and {1} pens is {2}", n, m, overall);
+
+
+    }
+}
